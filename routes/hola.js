ee062fe3bf1ea3df556e3e3de057491daa6381e5
gianmr28 <gianfrancopatoso@gmail.com>
2025-05-06 18:27:08 -0500

    h

--- routes/hola.js
+++ routes/hola.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Aquí irían tus rutas, por ejemplo:
 router.get('/', (req, res) => {
-  res.json({ message: 'Ruta de servicios está funcionando correctamente' });
+  res.json({ message: 'hola nataly te amo mucho c:' });
 });
 
 // Exportar el router para usarlo en index.js
